6a7b4f14f07988cee0ba9b4ea1f1b25125d781ee
Philip Abbet <philip.abbet@gmail.com>
2010-01-19 22:33:12 +0100

    [Data] Add the class 'DataStream'

---
+++ include/Athena-Core/Data/DataStream.h
@@ -0,0 +1,141 @@
+/** @file	DataStream.h
+	@author	Philip Abbet
+
+	Definition of the class 'Athena::Data::DataStream'
+*/
+
+#ifndef _ATHENA_DATA_DATASTREAM_H_
+#define _ATHENA_DATA_DATASTREAM_H_
+
+#include <Athena-Core/Prerequisites.h>
+
+
+namespace Athena {
+namespace Data {
+
+//----------------------------------------------------------------------------------------
+/// @brief	Base class for an object used to read and write data
+///
+/// This class provides basically the same methods than std::basic_istream, except that it
+/// does not have any formatting capabilities, and is designed to be subclassed to receive
+/// data from multiple sources, including libraries which have no compatibility with the
+/// STL's stream interfaces. As such, this is an abstraction of a set of wrapper classes
+/// which pretend to be standard stream classes but can actually be implemented quite
+/// differently.
+///
+/// @remark This mecanism is a simplified version of Ogre::DataStream from
+///         <a href="http://www.ogre3d.org/">Ogre3D</a>.
+//----------------------------------------------------------------------------------------
+class ATHENA_CORE_SYMBOL DataStream
+{
+	//_____ Construction / Destruction __________
+public:
+	DataStream() {};
+	virtual ~DataStream() {};
+
+
+	//_____ Methods to implement __________
+public:
+	//------------------------------------------------------------------------------------
+	/// @brief	Read the requisite number of bytes from the stream, stopping at the end of
+	///         the file
+	/// 
+	/// @param  buf     Reference to a buffer pointer
+	/// @param  count   Number of bytes to read
+	/// @return         The number of bytes read
+	//------------------------------------------------------------------------------------
+	virtual size_t read(void* buf, size_t count) = 0;
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Write the requisite number of bytes to the stream(only applicable to 
+	///     	streams that are not read-only)
+	/// 
+	/// @param  buf     Pointer to a buffer containing the bytes to write
+	/// @param  count   Number of bytes to write
+	/// @return         The number of bytes written
+	//------------------------------------------------------------------------------------
+	virtual size_t write(const void* buf, size_t count)
+	{
+		// Default to not supported
+		return 0;
+	}
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Skip a defined number of bytes. This can also be a negative value, in
+	///         which case the file pointer rewinds a defined number of bytes.
+	//------------------------------------------------------------------------------------
+	virtual void skip(long count) = 0;
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Repositions the read point to a specified byte
+	//------------------------------------------------------------------------------------
+    virtual void seek(size_t pos) = 0;
+	
+	//------------------------------------------------------------------------------------
+	/// @brief	Returns the current byte offset from beginning
+	//------------------------------------------------------------------------------------
+    virtual size_t tell() const = 0;
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Indicates if the stream has reached the end
+	//------------------------------------------------------------------------------------
+    virtual bool eof() const = 0;
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Close the stream; this makes further operations invalid.
+	//------------------------------------------------------------------------------------
+    virtual void close() = 0;
+
+
+	//_____ Methods __________
+public:
+	//------------------------------------------------------------------------------------
+	/// @brief	Get a single line from the stream
+	/// 
+	/// @remark The delimiter character is not included in the data returned, and it is
+	///         skipped over so the next read will occur after it. The buffer contents
+	///         will include a terminating character.
+	///
+	/// @remark To use this function, you <b>must</b> open the stream in <b>binary
+	///         mode</b>, otherwise, it'll produce unexpected results.
+	///
+	/// @param  buf         Reference to a buffer pointer
+	/// @param  maxCount    The maximum length of data to be read, excluding the
+	///                     terminating character
+	/// @param  delim       The delimiter(s) to stop at
+	/// @return             The number of bytes read, excluding the terminating character
+	//------------------------------------------------------------------------------------
+	virtual size_t readLine(char* buf, size_t maxCount, const std::string& delim = "\n");
+	
+	//------------------------------------------------------------------------------------
+	/// @brief	Returns a string containing the next line of data, optionally trimmed for
+	///         whitespace
+	/// 
+	/// @remark This is a convenience method for text streams only, allowing you to
+	///         retrieve a string object containing the next line of data. The data is
+    ///         read up to the next newline character and the result trimmed if required.
+	///
+	/// @remark To use this function, you <b>must</b> open the stream in <b>binary
+	///         mode</b>, otherwise, it'll produce unexpected results.
+	///
+	/// @param  trimAfter   If true, the line is trimmed for whitespace
+	/// @return             The next line
+	//------------------------------------------------------------------------------------
+    virtual std::string getLine(bool trimAfter = true);
+
+	//------------------------------------------------------------------------------------
+	/// @brief	Skip a single line from the stream
+	/// 
+	/// @remark To use this function, you <b>must</b> open the stream in <b>binary
+	///         mode</b>, otherwise, it'll produce unexpected results.
+	///
+	/// @param  delim   The delimiter(s) to stop at
+	/// @return         The number of bytes skipped
+	//------------------------------------------------------------------------------------
+	virtual size_t skipLine(const std::string& delim = "\n");
+};
+
+}
+}
+
+#endif
---
+++ src/Data/DataStream.cpp
@@ -0,0 +1,146 @@
+/**	@file	DataStream.cpp
+	@author	Philip Abbet
+
+	Implementation of the class 'Athena::Data::DataStream'
+*/
+
+#include <Athena-Core/Data/DataStream.h>
+#include <Athena-Core/Utils/StringUtils.h>
+
+using namespace Athena::Data;
+using namespace Athena::Utils;
+using namespace std;
+
+
+#define ATHENA_STREAM_TEMP_SIZE 128
+
+
+/*************************************** METHODS ****************************************/
+
+size_t DataStream::readLine(char* buf, size_t maxCount, const string& delim)
+{
+	// Deal with both Unix & Windows LFs
+	bool trimCR = false;
+	if (delim.find_first_of('\n') != string::npos)
+		trimCR = true;
+
+    char tmpBuf[ATHENA_STREAM_TEMP_SIZE];
+    size_t chunkSize = std::min(maxCount, (size_t) ATHENA_STREAM_TEMP_SIZE - 1);
+    size_t totalCount = 0;
+    size_t readCount;
+    
+    while (chunkSize && (readCount = read(tmpBuf, chunkSize)) != 0)
+    {
+        // Terminate
+        tmpBuf[readCount] = '\0';
+
+        // Find first delimiter
+        size_t pos = strcspn(tmpBuf, delim.c_str());
+
+        if (pos < readCount)
+        {
+            // Found terminator, reposition backwards
+            skip((long)(pos + 1 - readCount));
+        }
+
+        // Are we genuinely copying?
+        if (buf)
+            memcpy(buf+totalCount, tmpBuf, pos);
+
+        totalCount += pos;
+
+        if (pos < readCount)
+        {
+            // Trim off trailing CR if this was a CR/LF entry
+            if (trimCR && totalCount && buf[totalCount-1] == '\r')
+                --totalCount;
+
+            // Found terminator, break out
+            break;
+        }
+
+        // Adjust chunkSize for next time
+        chunkSize = std::min(maxCount - totalCount, (size_t) ATHENA_STREAM_TEMP_SIZE - 1);
+    }
+
+    // Terminate
+    buf[totalCount] = '\0';
+
+    return totalCount;
+}
+
+//-----------------------------------------------------------------------
+
+string DataStream::getLine(bool trimAfter)
+{
+    // Declarations
+    char tmpBuf[ATHENA_STREAM_TEMP_SIZE];
+    string retString;
+    size_t readCount;
+    
+    // Keep looping while not hitting delimiter
+    while ((readCount = read(tmpBuf, ATHENA_STREAM_TEMP_SIZE-1)) != 0)
+    {
+        // Terminate string
+        tmpBuf[readCount] = '\0';
+
+        char* p = strchr(tmpBuf, '\n');
+        if (p)
+        {
+            // Reposition backwards
+            skip((long)(p + 1 - tmpBuf - readCount));
+            *p = '\0';
+        }
+
+        retString += tmpBuf;
+
+        if (p)
+        {
+            // Trim off trailing CR if this was a CR/LF entry
+            if (retString.length() && retString[retString.length()-1] == '\r')
+                retString.erase(retString.length()-1, 1);
+
+            // Found terminator, break out
+            break;
+        }
+    }
+
+    if (trimAfter)
+        StringUtils::trim(retString);
+
+    return retString;
+}
+
+//-----------------------------------------------------------------------
+
+size_t DataStream::skipLine(const string& delim)
+{
+    char tmpBuf[ATHENA_STREAM_TEMP_SIZE];
+    size_t total = 0;
+    size_t readCount;
+    
+    // Keep looping while not hitting delimiter
+    while ((readCount = read(tmpBuf, ATHENA_STREAM_TEMP_SIZE - 1)) != 0)
+    {
+        // Terminate string
+        tmpBuf[readCount] = '\0';
+
+        // Find first delimiter
+        size_t pos = strcspn(tmpBuf, delim.c_str());
+
+        if (pos < readCount)
+        {
+            // Found terminator, reposition backwards
+            skip((long)(pos + 1 - readCount));
+
+            total += pos + 1;
+
+            // break out
+            break;
+        }
+
+        total += readCount;
+    }
+
+    return total;
+}
