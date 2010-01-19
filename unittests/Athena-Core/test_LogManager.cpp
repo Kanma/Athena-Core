f0cb7853ec8c45e920b753dff7aa527c93290c34
Philip Abbet <philip.abbet@gmail.com>
2010-01-19 22:31:00 +0100

    [UnitTests] Fix the unit tests for Log::LogManager

--- unittests/Athena-Core/test_LogManager.cpp
+++ unittests/Athena-Core/test_LogManager.cpp
@@ -6,6 +6,9 @@ using namespace Athena;
 using namespace Athena::Log;
 
 
+unsigned int CMockLogListener::uiInstances = 0;
+
+
 TEST(LogManager_Singleton)
 {
 	CHECK(!LogManager::getSingletonPtr());
@@ -36,94 +39,98 @@ struct LogEnvironment
 
 SUITE(LogManager_Listener)
 {
-	TEST_FIXTURE(LogEnvironment, OneListener)
-	{
-		CMockLogListener listener;
+    TEST_FIXTURE(LogEnvironment, OneListener)
+    {
+        CMockLogListener listener;
 
-		pLogManager->addListener(&listener);
+        pLogManager->addListener(&listener);
 
-		pLogManager->log(LOG_EVENT, "Test: OneListener", "This is a message",
-						 __FILE__, __FUNCTION__, 1234);
+        pLogManager->log(LOG_EVENT, "Test: OneListener", "This is a message",
+                      __FILE__, __FUNCTION__, 1234);
 
-		CHECK_EQUAL(LOG_EVENT,				listener.type);
-		CHECK_EQUAL("Test: OneListener",	listener.strContext);
-		CHECK_EQUAL("This is a message",	listener.strMessage);
-		CHECK_EQUAL(__FILE__,				listener.strFileName);
-		CHECK_EQUAL(__FUNCTION__,			listener.strFunction);
-		CHECK_EQUAL(1234,					listener.uiLine);
+        CHECK_EQUAL(LOG_EVENT,              listener.type);
+        CHECK_EQUAL("Test: OneListener",    listener.strContext);
+        CHECK_EQUAL("This is a message",    listener.strMessage);
+        CHECK_EQUAL(__FILE__,               listener.strFileName);
+        CHECK_EQUAL(__FUNCTION__,           listener.strFunction);
+        CHECK_EQUAL(1234,                   listener.uiLine);
 
-		pLogManager->removeListener(&listener);
-	}
+        pLogManager->removeListener(&listener);
+    }
+    
+    
+    TEST_FIXTURE(LogEnvironment, TwoListeners)
+    {
+        CMockLogListener listener1;
+        CMockLogListener listener2;
 
+        pLogManager->addListener(&listener1);
+        pLogManager->addListener(&listener2);
 
-	TEST_FIXTURE(LogEnvironment, TwoListeners)
-	{
-		CMockLogListener listener1;
-		CMockLogListener listener2;
+        pLogManager->log(LOG_EVENT, "Test: TwoListeners", "This is a message",
+                         __FILE__, __FUNCTION__, 1234);
 
-		pLogManager->addListener(&listener1);
-		pLogManager->addListener(&listener2);
+        CHECK_EQUAL(listener1.strTimestamp, listener2.strTimestamp);
+        CHECK_EQUAL(listener1.type,         listener2.type);
+        CHECK_EQUAL(listener1.strContext,   listener2.strContext);
+        CHECK_EQUAL(listener1.strMessage,   listener2.strMessage);
+        CHECK_EQUAL(listener1.strFileName,  listener2.strFileName);
+        CHECK_EQUAL(listener1.strFunction,  listener2.strFunction);
+        CHECK_EQUAL(listener1.uiLine,       listener2.uiLine);
 
-		pLogManager->log(LOG_EVENT, "Test: TwoListeners", "This is a message",
-						 __FILE__, __FUNCTION__, 1234);
+        pLogManager->removeListener(&listener1);
+        pLogManager->removeListener(&listener2);
+    }
+    
+    
+    TEST_FIXTURE(LogEnvironment, NoDestructionManagement)
+    {
+        CMockLogListener listener;
 
-		CHECK_EQUAL(listener1.strTimestamp,	listener2.strTimestamp);
-		CHECK_EQUAL(listener1.type,			listener2.type);
-		CHECK_EQUAL(listener1.strContext,	listener2.strContext);
-		CHECK_EQUAL(listener1.strMessage,	listener2.strMessage);
-		CHECK_EQUAL(listener1.strFileName,	listener2.strFileName);
-		CHECK_EQUAL(listener1.strFunction,	listener2.strFunction);
-		CHECK_EQUAL(listener1.uiLine,		listener2.uiLine);
+        pLogManager->addListener(&listener);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
 
-		pLogManager->removeListener(&listener1);
-		pLogManager->removeListener(&listener2);
-	}
+        pLogManager->removeListener(&listener);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
+    }
+    
+    
+    TEST_FIXTURE(LogEnvironment, NoDestructionManagement2)
+    {
+        CMockLogListener listener;
 
+        pLogManager->addListener(&listener);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
 
-	TEST_FIXTURE(LogEnvironment, NoDestructionManagement)
-	{
-		CMockLogListener listener;
+        delete pLogManager;
+        pLogManager = 0;
 
-		pLogManager->addListener(&listener);
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
+    }
+    
+    
+    TEST_FIXTURE(LogEnvironment, DestructionManagement)
+    {
+        CMockLogListener* pListener = new CMockLogListener();
 
-		pLogManager->removeListener(&listener);
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
-	}
+        pLogManager->addListener(pListener, true);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
 
-
-	TEST_FIXTURE(LogEnvironment, NoDestructionManagement2)
-	{
-		CMockLogListener listener;
-
-		pLogManager->addListener(&listener);
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
-
-		delete pLogManager;
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
-	}
-
-
-	TEST_FIXTURE(LogEnvironment, DestructionManagement)
-	{
-		CMockLogListener* pListener = new CMockLogListener();
-
-		pLogManager->addListener(pListener, true);
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
-
-		pLogManager->removeListener(pListener);
-		CHECK_EQUAL(0, CMockLogListener::uiInstances);
-	}
-
-
-	TEST_FIXTURE(LogEnvironment, DestructionManagement2)
-	{
-		CMockLogListener* pListener = new CMockLogListener();
-
-		pLogManager->addListener(pListener, true);
-		CHECK_EQUAL(1, CMockLogListener::uiInstances);
-
-		delete pLogManager;
-		CHECK_EQUAL(0, CMockLogListener::uiInstances);
-	}
+        pLogManager->removeListener(pListener);
+        CHECK_EQUAL(0, CMockLogListener::uiInstances);
+    }
+    
+    
+    TEST_FIXTURE(LogEnvironment, DestructionManagement2)
+    {
+        CMockLogListener* pListener = new CMockLogListener();
+    
+        pLogManager->addListener(pListener, true);
+        CHECK_EQUAL(1, CMockLogListener::uiInstances);
+    
+        delete pLogManager;
+        pLogManager = 0;
+     
+        CHECK_EQUAL(0, CMockLogListener::uiInstances);
+    }
 }
