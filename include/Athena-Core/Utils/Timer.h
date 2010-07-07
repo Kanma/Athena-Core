287658a46ab0579bb92eca0c778ac3d172538b2d
Philip Abbet <philip.abbet@gmail.com>
2010-07-07 09:49:57 +0200

    Use the same Timer implementation on Linux and MacOS X

--- include/Athena-Core/Utils/Timer.h
+++ include/Athena-Core/Utils/Timer.h
@@ -14,9 +14,7 @@
 // Bring in the specific platform's header file
 #if ATHENA_PLATFORM == ATHENA_PLATFORM_WIN32
     #include "WIN32/Timer.h"
-#elif ATHENA_PLATFORM == ATHENA_PLATFORM_LINUX
-    #include "GLX/Timer.h"
-#elif ATHENA_PLATFORM == ATHENA_PLATFORM_APPLE
+#elif (ATHENA_PLATFORM == ATHENA_PLATFORM_LINUX) || (ATHENA_PLATFORM == ATHENA_PLATFORM_APPLE)
     #include "OSX/Timer.h"
 #else
     #error Unknown platform
